8ac023d62c9fc26abf69701afdc14af16819c549
mbrandon <mbrandon@sesda2.com>
2011-05-18 11:30:48 -0400

    version 0.12

--- lib/Yars/Client.pm
+++ lib/Yars/Client.pm
@@ -15,7 +15,7 @@ use File::Spec;
 use Log::Log4perl qw(:easy);
 use Pod::Usage;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 # max downloads of 1 GB
 $ENV{MOJO_MAX_MESSAGE_SIZE} = 1073741824;
