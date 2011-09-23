13dda8561e7ba0d01a65600770a72d139c0464c6
Brian Duggan <brian.duggan@nasa.gov>
2011-09-23 10:21:44 -0400

    version 0.37

--- lib/Yars/Client.pm
+++ lib/Yars/Client.pm
@@ -16,7 +16,7 @@ use Log::Log4perl qw(:easy);
 use Data::Dumper;
 use feature qw(say);
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 # max downloads of 1 GB
 $ENV{MOJO_MAX_MESSAGE_SIZE} = 1073741824;
