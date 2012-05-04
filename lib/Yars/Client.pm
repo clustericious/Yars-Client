d5d306a8de78fcd7733a0e69df4872a777312625
Brian Duggan <brian.duggan@nasa.gov>
2012-05-04 12:35:48 -0400

    check_manifest should not read files

--- lib/Yars/Client.pm
+++ lib/Yars/Client.pm
@@ -48,6 +48,7 @@ route_meta 'check'          => { auto_failover => 1 };
 route_meta 'disk_usage'     => { auto_failover => 1 };
 route_meta 'set_status'     => { auto_failover => 1 };
 route_meta 'upload'         => { dont_read_files => 1 };
+route_meta 'check_manifest' => { dont_read_files => 1 };
 
 sub new {
     my $self = shift->SUPER::new(@_);
