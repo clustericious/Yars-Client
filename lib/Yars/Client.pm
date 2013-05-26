7dc37ec79a1a286aca6c233e902abbf9dc2d0724
Graham Ollis <plicease@cpan.org>
2013-05-25 23:16:01 -0400

    mojo 4.0 compat

--- lib/Yars/Client.pm
+++ lib/Yars/Client.pm
@@ -59,7 +59,11 @@ sub new {
     my $self = shift->SUPER::new(@_);
     $self->client->max_redirects(30);
     if($Mojolicious::VERSION < 4.0) {
-        Mojo::IOLoop::Stream->timeout(600);
+        # if Mojolicious.pm isn't loaded then we can't
+        # detect the version, so wrap this call in an
+        # eval since it will fail on newer mojos
+        eval { Mojo::IOLoop::Stream->timeout(600) }
+        # ignore the error.
     }
     $self->client->connect_timeout(30);
     return $self;
