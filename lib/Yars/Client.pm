6c7291cf193e6943e192892c1ad152c47e043da3
Graham Ollis <gollis@sesda2.com>
2013-10-31 14:20:12 -0400

    build release 0.93

--- lib/Yars/Client.pm
+++ lib/Yars/Client.pm
@@ -1,7 +1,7 @@
 package Yars::Client;
 
 # ABSTRACT: Yet Another RESTful-Archive Service Client
-our $VERSION = '0.92'; # VERSION
+our $VERSION = '0.93'; # VERSION
 
 use strict;
 use warnings;
@@ -427,7 +427,7 @@ Yars::Client - Yet Another RESTful-Archive Service Client
 
 =head1 VERSION
 
-version 0.92
+version 0.93
 
 =head1 SYNOPSIS
 
